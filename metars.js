71044edc79f6c2394192e81492639c2645f21a74
steven777400 <steve@kolls.net>
2016-03-12 09:58:35 -0800

    Added files via upload

---
+++ metars.js
@@ -0,0 +1,452 @@
+﻿// ** (c) Steve Kollmansberger, 2014
+
+var baseUrl = '//metars.mobi/';
+var searched = false;
+var KM_TO_NM = 0.539957;
+
+
+
+
+function getMETARsAndTAFs(murl, turl) {
+    return $.ajax(turl).pipe(function (turlData) {
+        return $.ajax(murl).pipe(function (murlData) {
+            return {
+                'METARs': murlData,
+                'TAFs': turlData
+            };
+        });
+    });
+}
+
+function getMETARsAndTAFsForIdents(idents) {
+    var murl = baseUrl + 'metars.php?ds=metars&idents=' + idents.join();
+    var turl = baseUrl + 'metars.php?ds=tafs&idents=' + idents.join();
+    return getMETARsAndTAFs(murl, turl);
+
+}
+
+function getMETARsAndTAFsForLoc(lat, lon) {
+    var murl = baseUrl + 'metars.php?ds=metars&loc=' + lon + ',' + lat;
+    var turl = baseUrl + 'metars.php?ds=tafs&loc=' + lon + ',' + lat;
+    return getMETARsAndTAFs(murl, turl);
+}
+
+// applies a regex to a string.  Each match, function f
+// is given the matching portion, and expected to return
+// an object containing before and after, strings which will be appended
+// before and after, respectively.
+function adorn(str, regex, f) {
+
+
+    return str.replace(regex, function repl(match) {
+        var fr = f(match);
+        return fr.before + match + fr.after;
+    });
+
+
+
+}
+
+function spancls(cls) {
+    if (cls)
+        return {
+            before: '<span class=' + cls + '>',
+            after: '</span>'
+        };
+    else
+        return {
+            before: '', after: ''
+        };
+
+}
+
+var coverages = ["FEW", "SCT", "BKN", "OVC", "VV"];
+
+function enhance(str, worst, station) {
+    // taf: line-breaks for TEMPO and FM and BECMG
+    var tafnl = /(TEMPO)|(FM\d+)|(BECMG)|(PROB\d\d)/g;
+
+    str = adorn(str, tafnl, function (m) {
+        return { before: '<br /><strong>', after: '</strong>' };
+    });
+
+    var vis = /[ ](M|P)?(\d[ ])?(\d\/)?\d+SM/g;
+    str = adorn(str, vis, function (m) {
+        var cls = "";
+        m = m.slice(1); // remove space from front
+        var worst_vis = m;
+
+        m = m.slice(0, m.length - 2); // remove SM from end
+
+
+        var worst_viscode = 0;
+
+        if (m.indexOf('P') > -1) {
+            worst_viscode = 100;
+            cls = ""; // vfr
+        } else if (m.indexOf('M') > -1) {
+            worst_viscode = 0.1; // can't use 0, 0 is a falsey value!
+            cls = "IFR";
+        } else if (m.indexOf('/') > -1) {
+            if (m.length == 3)
+                worst_viscode = parseInt(m[0], 10) / parseInt(m[2], 10);
+            else
+                worst_viscode = parseInt(m[0], 10) + parseInt(m[2], 10) / parseInt(m[4], 10);
+            cls = "IFR";
+        } else {
+            var visv = parseInt(m, 10);
+            worst_viscode = visv;
+
+            if (visv < 3)
+                cls = "IFR";
+            else if (visv <= 5)
+                cls = "MVFR";
+            else if (visv <= 6)
+                cls = "QVFR";
+
+        }
+
+
+        if (worst && (!worst.viscode || worst.viscode > worst_viscode)) {
+            worst.vis = worst_vis;
+            worst.viscode = worst_viscode;
+            worst.visstation = station;
+        }
+
+        return spancls(cls);
+
+    });
+
+    var cld = /(FEW|SCT|BKN|OVC|VV)\d{3}/g;
+    str = adorn(str, cld, function (m) {
+        var cls = "";
+        var lvl = parseInt(m.slice(m.length - 3, m.length), 10);
+        var coverage = m.slice(0, m.length - 3);
+
+        if (lvl < 20 && coverage === "SCT") {
+            cls = "QVFR";
+        }
+
+        if (coverage === "OVC" || coverage === "BKN") { // formal ceiling
+            if (lvl < 10)
+                cls = "IFR";
+            else if (lvl <= 30)
+                cls = "MVFR";
+
+
+        }
+
+        if (coverage === "VV")
+            cls = "IFR";
+
+        if (worst && (!worst[lvl] || coverages.indexOf(worst[lvl]) < coverages.indexOf(coverage))) {
+            worst[lvl] = coverage;
+            worst[lvl + "station"] = station;
+
+        }
+
+
+        return spancls(cls);
+
+    });
+
+    var wnd = /\d{3}\d{2}(G\d{2})?KT/g;
+    str = adorn(str, wnd, function (m) {
+        var cls = "";
+        var ws = 0;
+        if (m.indexOf('G') > -1)
+            ws = parseInt(m.slice(6, 8), 10);
+        else
+            ws = parseInt(m.slice(3, 5), 10);
+
+        if (ws >= 20)
+            cls = "QVFR";
+
+        if (worst && (!worst.windcode || worst.windcode < ws)) {
+            worst.windcode = ws;
+            worst.wind = m;
+            worst.windstation = station;
+        }
+
+        return spancls(cls);
+
+    });
+
+    // pad temp with spaces to avoid catching chunks of the taf hour blocks
+    var tempdew = /[ ](M)?\d{2}[/](M)?\d{2}[ ]/g;
+    str = adorn(str, tempdew, function (m) {
+        var cls = "";
+        
+        m = m.replace(/M/g, '-');
+        var slidx = m.indexOf('/');
+        var temp = parseInt(m.slice(0, slidx), 10);
+        var dew = parseInt(m.slice(slidx + 1), 10);
+
+        if (temp - dew <= 3)
+            cls = "QVFR";
+
+        return spancls(cls);
+    });
+
+    return str;
+}
+
+function worstCls(str) {
+    if (str.indexOf("IFR") > -1)
+        return "IFR";
+    if (str.indexOf("MVFR") > -1)
+        return "MVFR";
+    if (str.indexOf("QVFR") > -1)
+        return "QVFR";
+    return "VFR";
+}
+
+function distDir(latlon, origin) {
+
+
+    var item = {
+        distance: origin.distanceTo(latlon) * KM_TO_NM, 
+        direction: origin.bearingTo(latlon),
+    };
+    var dir = "";
+    var split = 45 / 2;
+
+    if (item.direction < 45 - split)
+        dir = "N";
+    else if (item.direction < 90 - split)
+        dir = "NE";
+    else if (item.direction < 135 - split)
+        dir = "E";
+    else if (item.direction < 180 - split)
+        dir = "SE";
+    else if (item.direction < 225 - split)
+        dir = "S";
+    else if (item.direction < 270 - split)
+        dir = "SW";
+    else if (item.direction < 315 - split)
+        dir = "W";
+    else if (item.direction < 360 - split)
+        dir = "NW";
+    else
+        dir = "N";
+
+    return Math.round(item.distance) + " nm " + dir;
+}
+
+function buildMETAR(worst) {
+    var str = "WORST ";
+    if (worst.windstation)
+        str += "<span class=worstStation>" + worst.windstation + "</span> ";
+    str += worst.wind;
+    str += " ";
+    if (worst.visstation)
+        str += "<span class=worstStation>" + worst.visstation + "</span> ";
+    str += worst.vis;
+    str += " ";
+
+    var cidx = 0;
+    for (var lvl = 1; lvl < 250; lvl++) {
+        if (worst[lvl] && cidx <= coverages.indexOf(worst[lvl])) {            
+
+            str += worst[lvl] + ("000" + lvl).slice(-3) + " ";
+            if (worst[lvl + "station"])
+                str += "<span class='worstStation lvl'>" + worst[lvl + "station"] + "</span> ";
+
+            cidx = coverages.indexOf(worst[lvl]);
+            if (worst[lvl] === "OVC" || worst[lvl] === "VV")
+                break; // hard ceiling
+        }
+        
+    }
+
+
+    return str.trim();
+
+}
+
+function formatDate(dt) {
+
+    var hr = dt.getUTCHours();
+    var min = dt.getUTCMinutes();
+
+    return ("00" + hr).slice(-2) + ":" + ("00" + min).slice(-2) + " Z";
+}
+
+function calculateAge(dt1, dt2) {
+
+
+    var diff = (dt1.getTime() - dt2.getTime());
+    
+    if (diff <= 0)
+        return "just now"; // future check
+
+    var hours = Math.floor(diff / (1000 * 60 * 60));
+    diff -= hours * (1000 * 60 * 60);
+
+    var mins = Math.floor(diff / (1000 * 60));
+    
+    if (hours > 0 && mins >= 30) {
+        hours++;
+        mins = 0;
+    }
+
+    if (hours > 23)
+        return "a day or more ago";
+
+    if (hours > 1)
+        return "about " + hours + " hours ago";
+
+    if (hours === 1)
+        return "about an hour ago";
+
+    if (mins > 1)
+        return "about " + mins + " minutes ago";
+
+    if (mins === 1)
+        return "about a minute ago";
+
+    return "just now";
+
+
+
+}
+
+function locate() {
+    viewModel.message("Awaiting GPS Location...");
+    viewModel.stations.removeAll();
+    viewModel.applyDistanceFilter(true);
+
+    if (navigator.geolocation) {
+        searched = false;
+        navigator.geolocation.getCurrentPosition(gotPosition);
+    } else {
+        viewModel.message("Geolocation is not supported by this browser.");
+    }
+}
+
+function gotPosition(loc) {
+    viewModel.myOrigin(new LatLon(loc.coords.latitude, loc.coords.longitude));
+    viewModel.message(null);
+    if (!searched) {
+        loadData(getMETARsAndTAFsForLoc(loc.coords.latitude, loc.coords.longitude));
+    }
+}
+
+function initData(data) {
+
+
+
+    // ARE YOU INSANE.  Service returns array if 2+ objects, but just the object if only 1
+    var metararr;
+    if (data.METARs.data.METAR instanceof Array)
+        metararr = data.METARs.data.METAR;
+    else if (!data.METARs.data.METAR) {
+        viewModel.message("No results");
+        metararr = [];
+    } else
+        metararr = new Array(data.METARs.data.METAR);
+
+    var tafarr;
+    if (data.TAFs.data.TAF instanceof Array)
+        tafarr = data.TAFs.data.TAF;
+    else if (!data.TAFs.data.TAF)
+        tafarr = [];
+    else
+        tafarr = new Array(data.TAFs.data.TAF);
+
+    viewModel.metararr(metararr);
+    viewModel.tafarr(tafarr);
+
+    displayData();
+}
+
+function displayData() {
+
+    var worst = {};
+    viewModel.stations.removeAll();
+
+    viewModel.metararr().forEach(function (metar) {
+
+        var result = {
+            id: metar.station_id,
+            metar: metar.raw_text,
+            latlon: new LatLon(metar.latitude, metar.longitude),
+            sectioncls: ''
+        };
+
+        if (viewModel.myOrigin()) {
+            result.distance = viewModel.myOrigin().distanceTo(result.latlon) * KM_TO_NM;
+        } else {
+            result.distance = 0; // not for display, just for sorting
+        }
+
+        if (!viewModel.applyDistanceFilter() ||
+            result.distance <= viewModel.range()) {
+
+            result.metarHtml = enhance(metar.raw_text, worst, result.id);
+            result.metarTime = new Date(metar.observation_time);
+
+            result.flightcls = worstCls(result.metarHtml);
+
+            var tafs = viewModel.tafarr().filter(function (taf) {
+                return taf.station_id === metar.station_id;
+            });
+
+            if (tafs.length > 0) {
+                result.taf = tafs[0].raw_text;
+                result.tafTime = new Date(tafs[0].issue_time);
+                result.tafHtml = enhance(result.taf);
+            }
+
+
+
+            viewModel.stations.push(result);
+        }
+
+    });
+
+    if (viewModel.stations().length > 1) {
+
+        viewModel.stations().sort(function compare(a, b) {
+            if (a.distance < b.distance) return -1;
+            else if (a.distance > b.distance) return 1;
+            else return 0;
+        });
+
+        var worstresult = {
+            id: 'WORST',
+            metar: buildMETAR(worst),
+            metarHtml: enhance(buildMETAR(worst)),
+            sectioncls: 'worst'
+
+        };
+        worstresult.flightcls = worstCls(worstresult.metarHtml)
+        viewModel.stations.unshift(worstresult);
+
+    }
+    $('[data-role=collapsible]').collapsible().trigger('create');
+
+}
+
+function loadData(f) {
+    searched = true;
+
+    viewModel.stations.removeAll();
+    viewModel.loadTime(new Date());
+
+    $.mobile.loading("show", {
+        text: "Loading..."
+    });
+
+    f.done(initData)
+     .fail(function (err) {
+         viewModel.loadTime(null);
+         viewModel.message("Error loading data - " + JSON.stringify(err));
+     })
+     .always(function () {
+         $.mobile.loading("hide");
+     });
+
+
+}
+
---
+++ tests.js
@@ -0,0 +1,283 @@
+﻿
+
+QUnit.test("worstCls", function (assert) {
+
+    assert.ok(worstCls(enhance(" 10SM CLR")) == "VFR");
+
+    assert.ok(worstCls(enhance(" 10SM CLR 01010KT")) == "VFR");
+    assert.ok(worstCls(enhance(" 10SM CLR 01025KT")) == "QVFR");
+    assert.ok(worstCls(enhance(" 10SM CLR 01015G25KT")) == "QVFR");
+
+    assert.ok(worstCls(enhance(" 6SM CLR")) == "QVFR");
+
+    assert.ok(worstCls(enhance(" 6SM FEW019")) == "QVFR");
+    assert.ok(worstCls(enhance(" 10SM FEW019")) == "VFR");
+    assert.ok(worstCls(enhance(" 10SM SCT019")) == "QVFR");
+
+    assert.ok(worstCls(enhance(" 10SM BKN020")) == "MVFR");
+
+    assert.ok(worstCls(enhance(" 10SM BKN040")) == "VFR");
+    assert.ok(worstCls(enhance(" 5SM BKN040")) == "MVFR");
+
+    assert.ok(worstCls(enhance(" 2SM BKN040")) == "IFR");
+    assert.ok(worstCls(enhance(" 5SM BKN005")) == "IFR");
+});
+
+QUnit.test("distance", function (assert) {
+    var origin = new LatLon(46.9694044, -122.9025447);
+
+    // KTIW 22 nm NE
+    var dest = new LatLon(47.2679444, -122.5781111);
+    var dist = distDir(dest, origin);
+    assert.ok(dist == "22 nm NE", dist);
+
+    // KCLS 18 nm S
+    var dest = new LatLon(46.6770278, -122.9827500);
+    var dist = distDir(dest, origin);
+    assert.ok(dist == "18 nm S", dist);
+
+});
+
+QUnit.test("worst", function (assert) {
+    var worst = {};
+    enhance("01005KT 10SM FEW003 SCT007", worst);
+    enhance("11015KT 5SM BKN003", worst);
+    enhance("09005G10KT 7SM OVC001", worst);
+
+    assert.ok(worst.vis === "5SM", worst.vis);
+    assert.ok(worst.wind === "11015KT", worst.wind);
+    assert.ok(worst[3] === "BKN", worst[3]);
+    assert.ok(worst[1] === "OVC", worst[1]);
+    assert.ok(worst[7] === "SCT", worst[7]);
+
+    enhance("09007G16KT 7SM OVC007", worst);
+    assert.ok(worst.vis === "5SM", worst.vis);
+    assert.ok(worst.wind === "09007G16KT", worst.wind);
+    assert.ok(worst[3] === "BKN", worst[3]);
+    assert.ok(worst[1] === "OVC", worst[1]);
+    assert.ok(worst[7] === "OVC", worst[7]);
+
+    var worst = {};
+    enhance("04005KT 9SM", worst);
+    enhance("04005KT 1/2SM FG", worst);
+    assert.ok(worst.vis === "1/2SM", worst.vis);
+    assert.ok(worst.wind === "04005KT", worst.wind);
+
+
+    var worst = {};
+    enhance("04005KT 1/2SM FG", worst);
+    enhance("04005KT 9SM", worst);    
+    assert.ok(worst.vis === "1/2SM", worst.vis);
+    assert.ok(worst.wind === "04005KT", worst.wind);
+
+    var worst = {};
+    enhance(" 1/2SM FG", worst);
+    enhance(" 1 1/2SM", worst);    
+    enhance(" 3/4SM FG", worst);
+    enhance(" 2 1/2SM", worst);
+    assert.ok(worst.vis === "1/2SM", worst.vis);
+
+    var worst = {};    
+    enhance(" 1 1/2SM", worst);
+    enhance(" 3/4SM FG", worst);
+    enhance(" 2 1/2SM", worst);
+    assert.ok(worst.vis === "3/4SM", worst.vis);
+
+    var worst = {};
+    enhance(" 1 1/2SM", worst);
+    enhance(" M1/4SM FG", worst);
+    enhance(" 2 1/2SM", worst);
+    assert.ok(worst.vis === "M1/4SM", worst.vis);
+
+    var worst = {};
+    enhance(" 1 1/2SM", worst);    
+    enhance(" 2 1/2SM", worst);
+    assert.ok(worst.vis === "1 1/2SM", worst.vis);
+    
+    var worst = {}
+    enhance("KOLM 021504Z AUTO 27003KT 3SM BR BKN024 OVC060 06/06 A3014 RMK AO2 T00610061", worst);
+    enhance("KGRF 021458Z AUTO 14005KT 10SM OVC028 07/07 A3013 RMK AO2 SLP208 T00700069 50005 PWINO $", worst);
+    assert.ok(worst.vis === "3SM", worst.vis);
+    enhance("KSHN 021524Z AUTO 00000KT 2 1/2SM BR OVC003 05/04 A3011 RMK AO2 VIS 1 1/4V4 T00500044 TSNO $", worst);
+    enhance("KGRF 021458Z AUTO 14005KT 10SM OVC028 07/07 A3013 RMK AO2 SLP208 T00700069 50005 PWINO $", worst);
+    assert.ok(worst.vis === "2 1/2SM", worst.vis);
+    enhance("KGRF 021458Z AUTO 14005KT 10SM OVC028 07/07 A3013 RMK AO2 SLP208 T00700069 50005 PWINO $", worst);
+    enhance("KTCM 021533Z AUTO 09002KT 2SM BR BKN031 06/06 A3014 RMK AO2 VIS 1/2V2 $", worst);
+    enhance("KGRF 021458Z AUTO 14005KT 10SM OVC028 07/07 A3013 RMK AO2 SLP208 T00700069 50005 PWINO $", worst);
+    assert.ok(worst.vis === "2SM", worst.vis);
+    enhance("KGRF 021458Z AUTO 14005KT 10SM OVC028 07/07 A3013 RMK AO2 SLP208 T00700069 50005 PWINO $", worst);
+    enhance("KPWT 021535Z AUTO 21003KT M1/4SM OVC002 06/06 A3015 RMK AO1", worst);
+    enhance("KGRF 021458Z AUTO 14005KT 10SM OVC028 07/07 A3013 RMK AO2 SLP208 T00700069 50005 PWINO $", worst);
+    assert.ok(worst.vis === "M1/4SM", worst.vis);
+    enhance("KHQM 021506Z AUTO 06004KT 5SM BR SCT014 SCT026 OVC047 08/08 A3011 RMK AO2 T00830078", worst);
+    assert.ok(worst.vis === "M1/4SM", worst.vis);
+
+
+});
+
+QUnit.test("build metar", function (assert) {
+
+    var str = buildMETAR({ wind : '01010KT', vis: '10SM', 90: 'BKN', 5: 'FEW' });
+    assert.ok(str == "WORST 01010KT 10SM FEW005 BKN090", str);
+
+    var str = buildMETAR({ wind: '01010G12KT', vis: '10SM' });
+    assert.ok(str == "WORST 01010G12KT 10SM", str);
+
+    var str = buildMETAR({ wind: '01010G12KT', vis: '10SM',2: 'VV' });
+    assert.ok(str == "WORST 01010G12KT 10SM VV002", str);
+
+    
+    var str = buildMETAR({ wind: '01010G12KT', vis: '10SM', 150: 'OVC', 50: 'FEW' });
+    assert.ok(str == "WORST 01010G12KT 10SM FEW050 OVC150", str);
+
+    // at same level, prefer lightly coverage followed by heaver coverage
+    var str = buildMETAR({ wind: '01010G12KT', vis: '10SM', 70: 'OVC' });
+    assert.ok(str == "WORST 01010G12KT 10SM OVC070", str);
+   
+
+    // lower cloud levels always preceed higher cloud levels
+    var str = buildMETAR({ wind: '01010G12KT', vis: '10SM', 150: 'BKN', 25: 'OVC' });
+    assert.ok(str == "WORST 01010G12KT 10SM OVC025", str);
+
+    var str = buildMETAR({ wind: '01010G12KT', vis: '10SM', 150: 'OVC', 25: 'BKN' });
+    assert.ok(str == "WORST 01010G12KT 10SM BKN025 OVC150", str);
+
+
+    // multiple of same kind allowed
+    var str = buildMETAR({ wind: '01010KT', vis: '10SM', 20: 'FEW', 30: 'FEW', 45: 'OVC' });
+    assert.ok(str == "WORST 01010KT 10SM FEW020 FEW030 OVC045", str);
+
+    // from bottom to top, once a cloud coverage type has appeared, never
+    // show a lighter type
+    var str = buildMETAR({ wind: '01010KT', vis: '10SM', 70: 'FEW', 150: 'SCT', 25: 'SCT' });
+    assert.ok(str == "WORST 01010KT 10SM SCT025 SCT150", str);
+
+    var str = buildMETAR({ wind: '01010KT', vis: '10SM', 70: 'SCT', 150: 'OVC', 25: 'BKN' });
+    assert.ok(str == "WORST 01010KT 10SM BKN025 OVC150", str);
+
+    // OVC and VV count as hard ceilings; don't show anything on top of that
+    var str = buildMETAR({ wind: '01010KT', vis: '10SM', 70: 'VV', 150: 'OVC', 25: 'OVC' });
+    assert.ok(str == "WORST 01010KT 10SM OVC025", str);
+
+    var str = buildMETAR({ wind: '01010KT', vis: '10SM', 70: 'SCT', 150: 'OVC', 25: 'VV' });
+    assert.ok(str == "WORST 01010KT 10SM VV025", str);
+    
+
+});
+
+
+QUnit.test("enhance tempdew", function (assert) {
+    var str = enhance(" 13/08 ");
+    assert.ok(str == " 13/08 ", "13/08");
+
+    var str = enhance(" 13/10 ");
+    assert.ok(str == "<span class=QVFR> 13/10 </span>", "13/10");
+
+    var str = enhance(" 02/M02 ");
+    assert.ok(str == " 02/M02 ", "02/M02");
+
+    var str = enhance(" 01/M01 ");
+    assert.ok(str == "<span class=QVFR> 01/M01 </span>", "01/M01");
+
+    var str = enhance(" M02/M06 ");
+    assert.ok(str == " M02/M06 ", "M02/M06");
+
+    var str = enhance(" M04/M06 ");
+    assert.ok(str == "<span class=QVFR> M04/M06 </span>", "M04/M06");
+
+    var str = enhance(" 12/11 ");
+    assert.ok(str == "<span class=QVFR> 12/11 </span>", "12/11");
+});
+
+QUnit.test("enhance vis", function (assert) {
+    var str = enhance(" 10SM");
+    assert.ok(str == " 10SM", str);
+
+    var str = enhance(" 6SM");
+    assert.ok(str == "<span class=QVFR> 6SM</span>", "6SM");
+
+    var str = enhance(" 5SM");
+    assert.ok(str == "<span class=MVFR> 5SM</span>", "5SM");
+
+    var str = enhance(" 3SM");
+    assert.ok(str == "<span class=MVFR> 3SM</span>", "3SM");
+
+    var str = enhance(" 2SM");
+    assert.ok(str == "<span class=IFR> 2SM</span>", "2SM");
+
+    var str = enhance(" 1SM");
+    assert.ok(str == "<span class=IFR> 1SM</span>", "1SM");
+
+    var str = enhance(" 1/2SM");
+    assert.ok(str == "<span class=IFR> 1/2SM</span>", "1/2SM");
+
+    var str = enhance(" 3/4SM");
+    assert.ok(str == "<span class=IFR> 3/4SM</span>", "3/4SM");
+
+    var str = enhance(" 2 1/2SM");
+    assert.ok(str == "<span class=IFR> 2 1/2SM</span>", "2 1/2SM");
+
+    var str = enhance(" 1 3/4SM");
+    assert.ok(str == "<span class=IFR> 1 3/4SM</span>", "1 3/4SM");
+
+    var str = enhance(" M1/4SM");
+    assert.ok(str == "<span class=IFR> M1/4SM</span>", "M1/4SM");
+
+    var str = enhance(" P6SM");
+    assert.ok(str == " P6SM", "P6SM");
+});
+
+
+
+
+QUnit.test("calculate age", function (assert) {
+    var dt = new Date();
+
+    dt.setTime((new Date()).getTime() + 300000);
+    var str = calculateAge(new Date(), dt);
+    assert.ok(str == "just now", str);
+
+    dt.setTime((new Date()).getTime() - 30000);
+    var str = calculateAge(new Date(), dt);
+    assert.ok(str == "just now", "30 secs");
+
+    dt.setTime((new Date()).getTime() - 80000);
+    var str = calculateAge(new Date(), dt);
+    assert.ok(str == "about a minute ago", "80 secs");
+
+    dt.setTime((new Date()).getTime() - 200000);
+    var str = calculateAge(new Date(), dt);
+    assert.ok(str == "about 3 minutes ago", "200 secs");
+
+    dt.setTime((new Date()).getTime() - 50 * 60 * 1000);
+    var str = calculateAge(new Date(), dt);
+    assert.ok(str == "about 50 minutes ago", "50 mins");
+
+    dt.setTime((new Date()).getTime() - 60 * 60 * 1000);
+    var str = calculateAge(new Date(), dt);
+    assert.ok(str == "about an hour ago", "1 hour");
+
+    dt.setTime((new Date()).getTime() - 80 * 60 * 1000);
+    var str = calculateAge(new Date(), dt);
+    assert.ok(str == "about an hour ago", "80 min");
+
+    dt.setTime((new Date()).getTime() - 100 * 60 * 1000);
+    var str = calculateAge(new Date(), dt);
+    assert.ok(str == "about 2 hours ago", "100 min");
+
+    dt.setTime((new Date()).getTime() - 130 * 60 * 1000);
+    var str = calculateAge(new Date(), dt);
+    assert.ok(str == "about 2 hours ago", str);
+
+    dt.setTime((new Date()).getTime() - 30 * 60 * 60 * 1000);
+    var str = calculateAge(new Date(), dt);
+    assert.ok(str == "a day or more ago", "30 hour");
+
+    dt.setTime((new Date()).getTime() - 60 * 60 * 60 * 1000);
+    var str = calculateAge(new Date(), dt);
+    assert.ok(str == "a day or more ago", "60 hour");
+
+});
+
+
+
